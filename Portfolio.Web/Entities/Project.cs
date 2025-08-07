89525ba60841b398e81e2c7674f1974477aea4f5
duygukayaa <dygkaya56@gmail.com>
2025-08-07 19:59:23 +0300

    Add project files.

---
+++ Portfolio.Web/Entities/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio.Web.Entities
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public int ProjectName { get; set; }
+        public int Description { get; set; }
+        public int ImageUrl { get; set; }
+        public int GithubUrl { get; set; }
+
+        //navigation property
+       // public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
