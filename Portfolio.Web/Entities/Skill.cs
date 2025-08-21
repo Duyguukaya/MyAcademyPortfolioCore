0a7e356f2fc99b050451e988b859290bf6b2d939
duygukayaa <dygkaya56@gmail.com>
2025-08-21 22:08:12 +0300

    .

---
+++ Portfolio.Web/Entities/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio.Web.Entities
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Percentage { get; set; }
+    }
+}
---
+++ Portfolio.Web/ViewComponents/Default-Index/_DefaultContactComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.Views.Default
+{
+    public class _DefaultContactComponent(PortfolioContext context):ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var contects = context.ContactInfos.ToList();
+            return View(contects);
+        }
+    }
+}
