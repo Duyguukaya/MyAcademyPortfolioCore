7565e0d7f1d221e441145d5bd9e644fee01d2862
duygukayaa <dygkaya56@gmail.com>
2025-08-31 21:22:00 +0300

    component yarısı bitti

---
+++ Portfolio.Web/Migrations/20250831180159_changeexpeience.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeexpeience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StartYear",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "StartYear",
+                table: "Experiences",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Portfolio.Web/ViewComponents/Default-Index/_DefaultAboutComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.ViewComponents.Default_Index
+{
+    public class _DefaultAboutComponent(PortfolioContext context):ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var abouts = context.Abouts.ToList();
+            return View(abouts);
+        }
+    }
+}
