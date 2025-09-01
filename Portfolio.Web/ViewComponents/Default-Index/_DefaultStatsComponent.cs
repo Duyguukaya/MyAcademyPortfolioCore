db0592d085fde75a3df1f4e356c390bc032a6d6b
duygukayaa <dygkaya56@gmail.com>
2025-09-01 20:00:56 +0300

    component bitti

---
+++ Portfolio.Web/ViewComponents/Default-Index/_DefaultStatsComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.ViewComponents.Default_Index
+{
+    public class _DefaultStatsComponent(PortfolioContext context) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.static1 = context.Projects.Count();
+            ViewBag.static2 = context.Experiences.Count();
+            ViewBag.static3 = context.Skills.Count();
+            ViewBag.static4 = context.Testimonials.Count();
+
+            return View();
+        }
+    }
+}
