11c6c297e3587809b51b52b3042fd08171142752
duygukayaa <dygkaya56@gmail.com>
2025-09-02 21:24:14 +0300

    .

--- Portfolio.Web/Controllers/AuthController.cs
+++ Portfolio.Web/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Portfolio.Web.Context;
 using Portfolio.Web.Entities;
@@ -8,6 +9,7 @@ using System.Security.Claims;
 
 namespace Portfolio.Web.Controllers
 {
+    [AllowAnonymous]
     public class AuthController(PortfolioContext context) : Controller
     {
         public IActionResult Login()
